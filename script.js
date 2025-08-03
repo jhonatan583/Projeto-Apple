8ba5f80efb080116e88b32e859beb4f06fdc8107
jhonatan583 <jhoninhaoficial@gmail.com>
2025-08-03 17:53:01 -0300

    iniciando JS

---
+++ script.js
@@ -0,0 +1,8 @@
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo-azul")
+    circulo.style.background = cor
+}
+
+function trocaImg(){
+  let imgIphone = document.querySelector(".iphone")
+}
